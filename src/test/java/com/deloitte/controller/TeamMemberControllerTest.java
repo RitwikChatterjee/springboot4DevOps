5cd7f532a1492a8424133c04aea18edc9b9b960a
Chatterjee <ritwchatterjee@deloitte.com>
2019-05-03 17:34:33 -0700

    Fixed JUnit testing

--- src/test/java/com/deloitte/controller/TeamMemberControllerTest.java
+++ src/test/java/com/deloitte/controller/TeamMemberControllerTest.java
@@ -1,6 +1,10 @@
 package com.deloitte.controller;
 
+import com.deloitte.entity.TeamMember;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,10 +13,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,6 +30,7 @@ public class TeamMemberControllerTest {
 	
 	@Autowired
 	private WebApplicationContext context;
+	ObjectMapper om = new ObjectMapper();
 	
 	@Before
 	public void setUp() throws Exception {
@@ -31,13 +40,14 @@ public class TeamMemberControllerTest {
 	@Test
 	public void testGetAllTeamMembers() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/teammembers").accept(MediaType.APPLICATION_JSON))
-		.andExpect(MockMvcResultMatchers.status().isOk());
+		.andExpect(status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$.*", Matchers.hasSize(3)));
 	}
 
 	@Test
 	public void testGetTeamMemberById() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/teammembers/1").accept(MediaType.APPLICATION_JSON))
-		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(status().isOk())
 		.andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is("Chintan")));
 	}
 
@@ -54,9 +64,26 @@ public class TeamMemberControllerTest {
 	}
 
 	@Test
-	public void testInsertTeamMember() {
-//		TODO: Write implementation
-//		fail("Not yet implemented");
+	public void testInsertTeamMember() throws Exception {
+		TeamMember teamMember = new TeamMember("Ritwik", "App lead");
+		String jsonRequest = om.writeValueAsString(teamMember);
+
+/*
+		MvcResult getAllMvcResultBefore = mockMvc.perform(MockMvcRequestBuilders.get("/teammembers").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andReturn();
+		String getAllContent = getAllMvcResultBefore.getResponse().getContentAsString();
+		TeamMember teamMembersBefore = om.readValue(getAllContent, TeamMember.class );
+*/
+
+
+		MvcResult mvcResult = mockMvc.perform(post("/teammembers").content(jsonRequest).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+
+/*
+		MvcResult getAllMvcResultAfter = mockMvc.perform(MockMvcRequestBuilders.get("/teammembers").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andReturn();
+		int getAllSizeAfter = getAllMvcResultAfter.getResponse().getContentLength();
+		Assert.assertEquals(getAllSizeBefore+1, getAllSizeAfter);
+*/
 	}
 
 }
