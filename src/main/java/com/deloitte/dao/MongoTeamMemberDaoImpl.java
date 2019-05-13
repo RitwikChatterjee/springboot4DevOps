85d40245760b4750d61011691ff44e8263227364
Ritwik Chatterjee <ritwchatterjee@deloitte.com>
2019-05-13 16:21:38 +0000

    Removed All student related code

---
+++ src/main/java/com/deloitte/dao/MongoTeamMemberDaoImpl.java
@@ -0,0 +1,41 @@
+package com.deloitte.dao;
+
+import com.deloitte.entity.TeamMember;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+@Profile("test")
+public class MongoTeamMemberDaoImpl implements TeamMemberDao {
+
+    @Autowired
+    private MongoTeamMemberRepo repo;
+
+    @Override
+    public Collection<TeamMember> getAllTeamMembers() {
+        return repo.findAll();
+    }
+
+    @Override
+    public TeamMember getTeamMemberById(String id) {
+        return repo.findById(id).get();
+    }
+
+    @Override
+    public void removeTeamMemberById(String id) {
+        repo.deleteById(id);
+    }
+
+    @Override
+    public void updateTeamMember(TeamMember teamMember) {
+        repo.save(teamMember);
+    }
+
+    @Override
+    public void insertTeamMemberToDb(TeamMember teamMember) {
+        repo.insert(teamMember);
+    }
+}
