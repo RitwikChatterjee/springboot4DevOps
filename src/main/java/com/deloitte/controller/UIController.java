dddd79e4a6dd56f6987c34308c476fd79e492221
Ritwik Chatterjee <ritwchatterjee@deloitte.com>
2019-05-13 21:54:56 +0000

    Updated to pick version from prop file

--- src/main/java/com/deloitte/controller/UIController.java
+++ src/main/java/com/deloitte/controller/UIController.java
@@ -3,6 +3,7 @@ package com.deloitte.controller;
 import com.deloitte.entity.TeamMember;
 import com.deloitte.service.TeamMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +16,19 @@ public class UIController {
     @Autowired
     TeamMemberService teamMemberService;
 
+    @Value("${uiVersion}")
+    String uiVersion;
+
     @RequestMapping(value="/teammembers", method = RequestMethod.GET)
     public String teamMembersList(Model model) {
+        model.addAttribute("uiVersion", uiVersion);
         model.addAttribute("teamMembersList", teamMemberService.getAllTeamMembers());
         return "teamMembersList";
     }
 
     @RequestMapping(value = "/teammembers-add", method = RequestMethod.GET)
     public String teamMembersAdd(Model model) {
+        model.addAttribute("uiVersion", uiVersion);
         return "teamMembersAdd";
     }
 
